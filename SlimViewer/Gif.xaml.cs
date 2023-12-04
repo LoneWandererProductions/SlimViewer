f781b0edeea47e29acf1ef311ff95b70b02dc68b
Wayfarer <petergeinitz@freenet.de>
2023-12-04 16:02:13 +0100

    fix bug

---
+++ SlimViewer/Gif.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace SlimViewer
+{
+    /// <inheritdoc cref="Window" />
+    /// <summary>
+    /// Gif creator window
+    /// </summary>
+    internal partial class Gif
+    {
+        public Gif()
+        {
+            InitializeComponent();
+        }
+    }
+}
