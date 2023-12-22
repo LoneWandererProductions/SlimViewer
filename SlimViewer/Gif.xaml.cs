406eaa45ab618b98d722ac1cc2a81f39293b4b67
Wayfarer <petergeinitz@freenet.de>
2023-12-22 17:33:54 +0100

    Some more work

--- SlimViewer/Gif.xaml.cs
+++ SlimViewer/Gif.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using CommonControls;
 
 namespace SlimViewer
 {
@@ -11,6 +12,16 @@ namespace SlimViewer
         public Gif()
         {
             InitializeComponent();
+            View.Image = ImageGif;
+        }
+
+        /// <summary>
+        ///     Thumbs the image clicked.
+        /// </summary>
+        /// <param name="itemId">The <see cref="ImageEventArgs" /> instance containing the event data.</param>
+        private void Thumb_ImageClicked(ImageEventArgs itemId)
+        {
+            View.ChangeImage(itemId.Id);
         }
     }
 }
--- SlimViewer/GifView.cs
+++ SlimViewer/GifView.cs
@@ -1,6 +1,125 @@
-﻿namespace SlimViewer
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Media.Imaging;
+using CommonControls;
+
+namespace SlimViewer
 {
-    internal sealed class GifView
+    /// <inheritdoc />
+    /// <summary>
+    ///     Gif Viewer
+    /// </summary>
+    /// <seealso cref="T:System.ComponentModel.INotifyPropertyChanged" />
+    public sealed class GifView : INotifyPropertyChanged
     {
+        /// <summary>
+        ///     The BMP
+        /// </summary>
+        private BitmapImage _bmp;
+
+        /// <summary>
+        ///     The current identifier
+        /// </summary>
+        private int _currentId;
+
+        /// <summary>
+        ///     The observer
+        /// </summary>
+        private Dictionary<int, string> _observer;
+
+        /// <summary>
+        ///     Gets or sets the image.
+        /// </summary>
+        /// <value>
+        ///     The image.
+        /// </value>
+        public ImageZoom Image { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the BitmapImage.
+        /// </summary>
+        /// <value>
+        ///     The BitmapImage.
+        /// </value>
+        public BitmapImage Bmp
+        {
+            get => _bmp;
+            set
+            {
+                if (_bmp == value) return;
+
+                _bmp = value;
+                OnPropertyChanged(nameof(Bmp));
+            }
+        }
+
+        /// <summary>
+        ///     Gets or sets the observer.
+        /// </summary>
+        /// <value>
+        ///     The observer.
+        /// </value>
+        public Dictionary<int, string> Observer
+        {
+            get => _observer;
+            set
+            {
+                if (_observer == value) return;
+
+                _observer = value;
+                OnPropertyChanged(nameof(Observer));
+            }
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     Triggers if an Attribute gets changed
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        ///     Called when [property changed].
+        /// </summary>
+        /// <param name="propertyName">Name of the property.</param>
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        ///     Gets a value indicating whether this instance can execute.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns>
+        ///     <c>true</c> if this instance can execute the specified object; otherwise, <c>false</c>.
+        /// </returns>
+        /// <value>
+        ///     <c>true</c> if this instance can execute; otherwise, <c>false</c>.
+        /// </value>
+        private bool CanExecute(object obj)
+        {
+            // check if executing is allowed, not used right now
+            return true;
+        }
+
+        /// <summary>
+        ///     Changes the image.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        public void ChangeImage(int id)
+        {
+            if (!Observer.ContainsKey(id)) return;
+
+            _currentId = id;
+
+            var filePath = Observer[id];
+            GenerateView(filePath);
+        }
+
+        private void GenerateView(string filePath)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
