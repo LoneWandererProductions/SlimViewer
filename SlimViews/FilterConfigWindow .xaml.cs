3189205d1d9537cbb6dc8ce3a508e52a4044377e
LoneWandererProductions <petergeinitz@freenet.de>
2024-08-28 12:59:23 +0200

    fix some stuff, at least the build

---
+++ SlimViews/FilterConfigWindow .xaml.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * COPYRIGHT:   See COPYING in the top level directory
+ * PROJECT:     SlimViewer
+ * FILE:        SlimViews/Converter.xaml.cs
+ * PURPOSE:     Basic Converter Window
+ * PROGRAMER:   Peter Geinitz (Wayfarer)
+ */
+
+using System.Windows;
+
+namespace SlimViews
+{
+    /// <summary>
+    ///     Window for converting all Files
+    /// </summary>
+    /// <seealso cref="Window" />
+    /// <seealso cref="System.Windows.Markup.IComponentConnector" />
+    /// <inheritdoc cref="Window" />
+    internal sealed partial class FilterConfigWindow
+	{
+        /// <inheritdoc />
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Converter" /> class.
+        /// </summary>
+        public FilterConfigWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
