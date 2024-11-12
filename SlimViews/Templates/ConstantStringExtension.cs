c708987d369454f3d208b882e5532b6a673d8d9d
LoneWandererProductions <petergeinitz@freenet.de>
2024-11-12 16:08:49 +0100

    prepare a cleaner way to handle strings

---
+++ SlimViews/Templates/ConstantStringExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace SlimViews.Templates
+{
+    public class ConstantStringExtension : MarkupExtension
+    {
+        public string Key { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return typeof(ConstantStrings).GetField(Key)?.GetValue(null);
+        }
+    }
+}
--- SlimViews/Translator.cs
+++ SlimViews/Translator.cs
@@ -1,9 +1,23 @@
 ﻿using CommonControls;
+using System;
+using System.Windows;
 
 namespace SlimViews
 {
     internal static class Translator
     {
+
+        private static readonly ResourceDictionary _resourceDictionary;
+
+        static Translator()
+        {
+            _resourceDictionary = new ResourceDictionary
+            {
+                Source = new Uri("pack://application:,,,/SlimViews;component/Templates/ToolOptionsTemplates.xaml", UriKind.RelativeOrAbsolute)
+            };
+        }
+
+
         internal static SelectionTools GetToolsFromString(string command)
         {
             switch (command)
