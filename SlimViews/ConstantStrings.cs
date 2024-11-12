4ec0e463a95164dc312035febeb2cd730c3dd666
Wayfarer <petergeinitz@freenet.de>
2024-11-12 20:59:05 +0100

    prepare some stuff

---
+++ SlimViews/ConstantStrings.cs
@@ -0,0 +1,70 @@
+﻿namespace SlimViews
+{
+    public static class ConstantStrings
+    {
+        // Selection Tool Strings
+        public const string RectangleText = "Rectangle";
+        public const string EllipseText = "Ellipse";
+        public const string FreeFormText = "Free Form";
+
+        // Texture Strings
+        public const string TextureNoise = "Noise";
+        public const string TextureClouds = "Clouds";
+        public const string TextureMarble = "Marble";
+        public const string TextureWood = "Wood";
+        public const string TextureWave = "Wave";
+        public const string TextureCrosshatch = "Crosshatch";
+
+        // Filter Strings
+        public const string FilterNone = "None";
+        public const string FilterGrayScale = "GrayScale";
+        public const string FilterInvert = "Invert";
+        public const string FilterSepia = "Sepia";
+
+        internal const string FilterBlackAndWhite = "Black and White";
+
+        internal const string FilterPolaroid = "Polaroid";
+
+        internal const string FilterContour = "Contour";
+
+        internal const string FilterBrightness = "Brightness";
+
+        internal const string FilterContrast = "Contrast";
+
+        internal const string FilterHueShift = "Hue Shift";
+
+        internal const string FilterColorBalance = "Color Balance";
+
+        internal const string FilterVintage = "Vintage";
+
+        internal const string FilterSharpen = "Sharpen";
+
+        internal const string FilterGaussianBlur = "Gaussian Blur";
+
+        internal const string FilterEmboss = "Emboss";
+
+        internal const string FilterBoxBlur = "Box Blur";
+
+        internal const string FilterLaplacian = "Laplacian";
+
+        internal const string FilterEdgeEnhance = "Edge Enhance";
+
+        internal const string FilterMotionBlur = "Motion Blur";
+
+        internal const string FilterUnsharpMask = "Unsharp Mask";
+
+        internal const string FilterDifferenceOfGaussians = "Difference of Gaussians";
+
+        internal const string FilterCrosshatch = "Crosshatch";
+
+        internal const string FilterFloydSteinbergDithering = "Floyd Steinberg Dithering";
+
+        internal const string FilterAnisotropicKuwahara = "Anisotropic Kuwahara";
+
+        internal const string FilterSupersamplingAntialiasing = "Supersampling Antialiasing";
+
+        internal const string FilterPostProcessingAntialiasing = "Post Processing Antialiasing";
+
+        internal const string FilterPencilSketchEffect = "Pencil Sketch Effect";
+    }
+}
